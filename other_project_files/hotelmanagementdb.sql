ac10d8aa23b0f5b45421bfdbdb8a9448f965f160
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2022-04-01 20:36:55 +0000

    cmt

---
+++ other_project_files/hotelmanagementdb.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 01 Avril 2022 à 22:33
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `hotelmanagementdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE IF NOT EXISTS `client` (
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `id` varchar(45) NOT NULL,
+  `phoneNumber` int(11) NOT NULL,
+  `birthDate` date NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `room`
+--
+
+CREATE TABLE IF NOT EXISTS `room` (
+  `id` varchar(45) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `type` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transaction`
+--
+
+CREATE TABLE IF NOT EXISTS `transaction` (
+  `id_client` varchar(45) NOT NULL,
+  `id_receptionnist` varchar(45) NOT NULL,
+  `id_room` varchar(45) NOT NULL,
+  `date` date NOT NULL,
+  `nature` varchar(45) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id_client`,`id_receptionnist`,`id_room`,`date`,`nature`),
+  KEY `id_client` (`id_client`),
+  KEY `id_receptionnist` (`id_receptionnist`),
+  KEY `room_fk` (`id_room`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `worker`
+--
+
+CREATE TABLE IF NOT EXISTS `worker` (
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `id` varchar(45) NOT NULL,
+  `phoneNumber` int(11) NOT NULL,
+  `birthDate` date NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `function` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `worker`
+--
+
+INSERT INTO `worker` (`firstName`, `lastName`, `id`, `phoneNumber`, `birthDate`, `email`, `function`, `password`) VALUES
+('Kibalo Jules', 'Tinaka', 'eb416362', 608917921, '2000-01-01', 'julestnk.dev@gmail.com', 'receptionnist', 'Enchantress***912');
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `room_fk` FOREIGN KEY (`id_room`) REFERENCES `room` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `client_fk` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `worker_fk` FOREIGN KEY (`id_receptionnist`) REFERENCES `worker` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/main/java/com/example/gestionhotel/Main.java
+++ src/main/java/com/example/gestionhotel/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -12,6 +13,8 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        stage.setMaximized(true);
         stage.setTitle("Main");
         stage.setScene(scene);
         stage.show();
