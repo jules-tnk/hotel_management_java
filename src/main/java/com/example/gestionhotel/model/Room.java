5bf037d13d37507bc5006a5d4c399dc844049cf0
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2022-04-07 12:02:32 +0000

    Update Room

--- src/main/java/com/example/gestionhotel/model/Room.java
+++ src/main/java/com/example/gestionhotel/model/Room.java
@@ -4,15 +4,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Room {
-    //CONNECTOR TO THE DATABASE
-    DbConnector roomDbConnector = new DbConnector();
-
     //ATTRIBUTES
     private String id;
     private double price;
     private String type;
     private boolean available;
-
 
     //GETTERS
     public String getId() {
@@ -41,31 +37,15 @@ public class Room {
     public void setAvailablity(boolean available) { this.available = available; }
 
     //CONSTRUCTOR
-    public Room(String room_id) {
-        String request = String.format("SELECT * FROM room WHERE id=\"%s\";", room_id);
-        ResultSet result = roomDbConnector.executeRequest(request);
-        try {
-            while (result.next()) {
-                setId(result.getString("id"));
-                setPrice(result.getDouble("price"));
-                setType(result.getString("type"));
-                setAvailablity(result.getBoolean("available"));
-            }
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+
+
+    public Room(String id, double price, String type, boolean available) {
+        setId(id);
+        setPrice(price);
+        setType(type);
+        setAvailablity(available);
     }
 
-    //DATABASE METHODS
-    public void addToDatabase(){}
-    public void updateInDatabase(){}
-    public void removeFromDatabase(){}
-
     public static void main(String[] args) {
-        Room room = new Room("c32");
-        System.out.println(room.getId());
-        System.out.println(room.getPrice());
-        System.out.println(room.isAvailable());
-        System.out.println(room.getType());
     }
 }
