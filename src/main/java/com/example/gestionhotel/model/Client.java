a5401fbb9c1cdd6581b2e11c32d2ab7b42d5816d
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2022-04-07 12:41:38 +0000

    Update Client and transaction

--- src/main/java/com/example/gestionhotel/model/Client.java
+++ src/main/java/com/example/gestionhotel/model/Client.java
@@ -2,33 +2,8 @@ package com.example.gestionhotel.model;
 import java.sql.*;
 
 public class Client extends Person {
-    DbConnector clientDbConnector = new DbConnector();
-
     //CONSTRUCTORS
-    public Client(String client_id) {
-        String request = String.format("SELECT * FROM client WHERE id=\"%s\";", client_id);
-        ResultSet result = clientDbConnector.executeRequest(request);
-        try {
-            while (result.next()) {
-                setId(result.getString("id"));
-                setFirstName(result.getString("firstName"));
-                setLastName(result.getString("lastName"));
-                setEmail(result.getString("email"));
-                setBirthDate(String.valueOf(result.getDate("birthDate")));
-                setPhoneNumber(result.getInt("phoneNumber"));
-            }
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    //DATABASE METHODS
-    public void addToDatabase(){}
-    public void updateInDatabase(){}
-    public void removeFromDatabase(){}
-
-    public static void main(String[] args) {
-        Client client = new Client("tl589621");
-        System.out.println(client.getFirstName());
+    public Client(String firstName, String lastName, String id, String email, int phoneNumber, Date birthDate) {
+        super(firstName, lastName, id, email, phoneNumber, birthDate);
     }
 }
--- src/main/java/com/example/gestionhotel/model/Transaction.java
+++ src/main/java/com/example/gestionhotel/model/Transaction.java
@@ -1,19 +1,17 @@
 package com.example.gestionhotel.model;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Transaction {
-    //CONNECTOR TO DATABASE
-    DbConnector transactionDbConnector = new DbConnector();
-
     //ATTRIBUTES
     private String id_transaction;
     private String id_client;
     private String id_receptionist;
     private String id_room;
     private String nature;
-    private String date;
+    private Date date;
     private double price;
     //private int numberOfDays;
 
@@ -30,7 +28,7 @@ public class Transaction {
     public String getNature() {
         return nature;
     }
-    public String getDate() {
+    public Date getDate() {
         return date;
     }
     public double getPrice() {
@@ -39,51 +37,24 @@ public class Transaction {
     public String getId_transaction() { return id_transaction; }
 
     //SETTERS
-    public void setId_client(String id_cient) {
-        this.id_client = id_cient;
-    }
-    public void setId_receptionnist(String id_receptionnist) {
-        this.id_receptionist = id_receptionnist;
-    }
-    public void setId_room(String id_room) {
-        this.id_room = id_room;
-    }
-    public void setNature(String nature) {
-        this.nature = nature;
-    }
-    public void setDate(String date) {
-        this.date = date;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    public void setId_client(String id_cient) {this.id_client = id_cient; }
+    public void setId_receptionnist(String id_receptionnist) { this.id_receptionist = id_receptionnist; }
+    public void setId_room(String id_room) { this.id_room = id_room; }
+    public void setNature(String nature) { this.nature = nature; }
+    public void setDate(Date date) { this.date = date; }
+    public void setPrice(double price) { this.price = price; }
     public void setId_transaction(String id_transaction) { this.id_transaction = id_transaction; }
 
-    public Transaction(String transaction_id) {
-        String request = String.format("SELECT * FROM transaction WHERE id_transaction=\"%s\";", transaction_id);
-        ResultSet result = transactionDbConnector.executeRequest(request);
-        try {
-            while (result.next()) {
-                setId_transaction(result.getString("id_transaction"));
-                setId_client(result.getString("id_client"));
-                setId_room(result.getString("id_room"));
-                setId_receptionnist(result.getString("id_receptionist"));
-                setDate(result.getString("date"));
-                setPrice(result.getDouble("price"));
-                setNature(result.getString("nature"));
-            }
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+    //CONSTRUCTORS
+    public Transaction(String id_transaction, String id_client, String id_receptionist, String id_room, String nature, Date date, double price) {
+        setId_client(id_client);
+        setId_receptionnist(id_receptionist);
+        setId_room(id_room);
+        setNature(nature);
+        setDate(date);
+        setPrice(price);setId_transaction(id_transaction);
     }
 
-    //DATABASE METHODS
-    public void addToDatabase(){}
-    public void updateInDatabase(){}
-    public void removeFromDatabase(){}
-
     public static void main(String[] args) {
-        Transaction transaction = new Transaction("p45");
-        System.out.println(transaction.getNature());
     }
 }
